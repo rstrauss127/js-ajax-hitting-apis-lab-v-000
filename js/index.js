6ad92b96b4880bddad0e9870f114fffd1a99a492
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-19 22:06:58 +0000

    getCommits working and displayCommits is console.logging the

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
-  const username = document.getElementById('username').value
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/' + username +'/repos');
+  req.open('GET', 'https://api.github.com/users/' + document.getElementById('username').value +'/repos');
   req.send();
 }
 
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
-  console.log(repos);
   const repoList = `<ul>${repos
     .map(
       r =>
         '<li>' +
-        r.name +
-        ' - <a href="#" data-repo="' +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+          '<a href=' +
+              r.html_url +
+          '>' +
+              r.name +
+          '</a> - ' +
+          '<a href="#" data-repo="' +
+              r.name +
+          '"onclick=getCommits(this)>Get Commits</a></li>'
     )
-  .join('')
-  }</ul>`;
+    .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
@@ -27,12 +29,12 @@ function getCommits(el) {
   const username = document.getElementById('username').value
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/' + username + name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/' + username + '/' + name + '/commits');
   req.send();
 }
 
 function displayCommits() {
-
+  console.log(this.responseText)
 }
 
 function getBranches() {

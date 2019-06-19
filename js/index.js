e5698ec62072c9b1feb143c50ab5a3ac187c9259
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-19 18:45:00 +0000

    Can get and display Repositories correctly. Next: Work on getCommits and DisplayCommits

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-// your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  const username = document.getElementById('username').value
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/' + username +'/repos');
+  req.send();
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+  .join('')
+  }</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const username = document.getElementById('username').value
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/' + username + name + '/commits');
+  req.send();
+}
+
+function displayCommits() {
+
+}
+
+function getBranches() {
+
+}
+function displayBranches() {
+
+}

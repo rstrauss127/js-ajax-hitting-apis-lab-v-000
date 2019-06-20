7d3d395862503c1a480c7e9119b759e426e833a3
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-20 19:00:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -60,6 +60,7 @@ function getBranches(el) {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayBranches);
   req.open('GET', 'https://api.github.com/repos/' + username + '/' + name + '/branches');
+  req.send();
 }
 function displayBranches() {
   console.log(this.responseText);
